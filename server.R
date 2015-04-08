e69650f22aacdb0c67d6744ff9e076351504a49c
shuangsong <shuangsong6@gmail.com>
2015-04-08 16:45:02 -0400

    version 4.8

--- server.R
+++ server.R
@@ -3,11 +3,11 @@ df_unigram<-read.csv(paste(getwd(), "/data/df_unigram.csv", sep=""))
 df_bigram<-read.csv(paste(getwd(), "/data/df_bigram.csv", sep=""))
 df_trigram<-read.csv(paste(getwd(), "/data/df_trigram.csv", sep=""))
 df_quagram<-read.csv(paste(getwd(), "/data/df_quagram.csv", sep=""))
-
+# add prediction function : (bigram, trigra, and quagram)
 predict_bigram<- function(input) {
-        if (length(input)<=1){
+        if (sapply(gregexpr("\\S+", input), length)<=1){
                 return("please enter some words")
-        }
+        }else {
         input1<-tolower(input)
         input2<-str_replace_all(input1, pattern="[[:punct:]]","") #remove punctuations
         input3<-str_replace_all(input2, pattern="\\s+", " ") # replace whitespace with space
@@ -16,7 +16,7 @@ predict_bigram<- function(input) {
         start_with_term<-paste("^","\\b", clean_tail,"\\b", sep="")
         find<-df_bigram[grep(start_with_term, df_bigram$terms),]
         if (nrow(find)==0) {
-                warning("There is no prediction in bigram data frame")
+                #warning("There is no prediction in bigram data frame")
                 return("no prediction")
         }
         find<-df_bigram[grep(start_with_term, df_bigram$terms),]
@@ -26,7 +26,7 @@ predict_bigram<- function(input) {
         find$pred<-find_mat[,2]
         find2<-df_unigram[grep(paste("^","\\b", clean_tail,"\\b", sep=""), df_unigram$terms),]
         if (nrow(find2)==0){
-                warning("There is no prediction in unigram data frame")
+                #warning("There is no prediction in unigram data frame")
                 return("no prediction")
         }
         find2<-data.frame(find2)
@@ -36,13 +36,14 @@ predict_bigram<- function(input) {
         }
         high_prob<-merge[order(-merge$pred_prob),]
         possible_term<-as.character(high_prob$pred)
-        #possible_term<-data.frame(high_prob)
+        #possible_term<-data.frame(possible_term)
         return(possible_term)
-}
+}}
+
 predict_trigram<-function(input){
-        if (length(input)<=1){
+        if (sapply(gregexpr("\\S+", input), length)<=1){
                 return("please enter some words")
-        }
+        }else {
         input1<-tolower(input)
         input2<-str_replace_all(input1, pattern="[[:punct:]]","") #remove punctuations
         input3<-str_replace_all(input2, pattern="\\s+", " ") # replace whitespace with space
@@ -52,7 +53,7 @@ predict_trigram<-function(input){
         start_with_term<-paste("^","\\b", words,"\\b", sep="")
         find<-df_trigram[grep(start_with_term, df_trigram$terms),]
         if (nrow(find)==0) {
-                warning("There is no prediction in trigram data frame")
+                #warning("There is no prediction in trigram data frame")
                 return("no prediction")
         }
         merge<-NULL
@@ -62,7 +63,7 @@ predict_trigram<-function(input){
         find2<-df_bigram[grep(paste("^","\\b",words,"\\b", sep=""), df_bigram$terms),]
         find2<-data.frame(find2)
         if (nrow(find2)==0) {
-                warning("There is no prediction in bigram data frame")
+                #warning("There is no prediction in bigram data frame")
                 return("no prediction")
         }
         for (i in 1: nrow(find)) {
@@ -72,13 +73,13 @@ predict_trigram<-function(input){
         high_prob<-merge[order(-merge$pred_prob),]
         possible_term<-as.character(high_prob$pred)
         return(possible_term)
-}
+}}
 
 
 predict_quagram<-function(input){
-        if (length(input)<=1){
+        if (sapply(gregexpr("\\S+", input), length)<=1){
                 return("please enter some words")
-        }
+        }else {
         input1<-tolower(input)
         input2<-str_replace_all(input1, pattern="[[:punct:]]","") #remove punctuations
         input3<-str_replace_all(input2, pattern="\\s+", " ") # replace whitespace with space
@@ -88,7 +89,7 @@ predict_quagram<-function(input){
         start_with_term<-paste("^","\\b",words,"\\b", sep="")
         find<-df_quagram[grep(start_with_term, df_quagram$terms),]
         if (nrow(find)==0) {
-                warning("There is no prediction in quagram data frame")
+                #warning("There is no prediction in quagram data frame")
                 return("no prediction")
         }
         merge<-NULL
@@ -98,7 +99,7 @@ predict_quagram<-function(input){
         find2<-df_trigram[grep(paste("^","\\b",words,"\\b", sep=""), df_trigram$terms),]
         find2<-data.frame(find2)
         if (nrow(find2)==0) {
-                warning("There is no prediction in trigram data frame")
+                #warning("There is no prediction in trigram data frame")
                 return("no prediction")
         }
         for (i in 1: nrow(find)) {
@@ -108,7 +109,8 @@ predict_quagram<-function(input){
         high_prob<-merge[order(-merge$pred_prob),]
         possible_term<-as.character(high_prob$pred)
         return(possible_term)
-}
+}}
+
 #create a funtion that can use to identify radiobutton while do the prediction: 
 nextword<-function(radiobutton, inputText,input){
         if (input$radiobutton=="bi"){
@@ -117,20 +119,18 @@ nextword<-function(radiobutton, inputText,input){
                 predict_trigram(input$text)
         }else if(input$radiobutton=="qua"){
                 predict_quagram(input$text)
-        }else{
-             "no prediction, please type some words to predict"   
-        }
-}
+        
+}}
 
 
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output,session) {
         radiobutton<-reactive({input$radiobutton})
         inputText<-reactive({input$text})
         #output$radiobutton<-render({radiobutton()})
         output$textoutput<-renderText({
                 word<-nextword(radiobutton(),inputText,input)
-                paste0("The next word is:", word, sep=" ")
+                word
         })
 })
 
