ef1d767ca894133aef279ae4480195c45e12f1ee
shuangsong <shuangsong6@gmail.com>
2015-04-08 15:09:49 -0400

    version 4.8

--- server.R
+++ server.R
@@ -1,36 +1,137 @@
-
-library(tm) 
-library(NLP) # for natural language processing
-library(stringr) # package for handling string in R
-library(RWeka) #for n-gram model
-library(ngram) # for n-grams model
 library(shiny)
-setwd("C:/Users/stephanie song/Desktop/final/en_US")
-source(paste(getwd(), "prediction_lib(edited).R", sep="/")) # win8pro 
-source(paste(getwd(), "clean_df.R", sep="/"))
-load(paste(getwd(), "df_unigram.RData", sep="/"))
-load(paste(getwd(), "df_bigram.RData", sep="/"))
-load(paste(getwd(), "df_trigram.RData", sep="/"))
-load(paste(getwd(), "df_quagram.RData", sep="/"))
+df_unigram<-read.csv(paste(getwd(), "/data/df_unigram.csv", sep=""))
+df_bigram<-read.csv(paste(getwd(), "/data/df_bigram.csv", sep=""))
+df_trigram<-read.csv(paste(getwd(), "/data/df_trigram.csv", sep=""))
+df_quagram<-read.csv(paste(getwd(), "/data/df_quagram.csv", sep=""))
+
+predict_bigram<- function(input) {
+        if (length(input)<=1){
+                return("please enter some words")
+        }
+        input1<-tolower(input)
+        input2<-str_replace_all(input1, pattern="[[:punct:]]","") #remove punctuations
+        input3<-str_replace_all(input2, pattern="\\s+", " ") # replace whitespace with space
+        input_clean<-removeNumbers(input3) 
+        clean_tail<-tail(unlist(strsplit(input_clean, " ")), 1)
+        start_with_term<-paste("^","\\b", clean_tail,"\\b", sep="")
+        find<-df_bigram[grep(start_with_term, df_bigram$terms),]
+        if (nrow(find)==0) {
+                warning("There is no prediction in bigram data frame")
+                return("no prediction")
+        }
+        find<-df_bigram[grep(start_with_term, df_bigram$terms),]
+        merge<-NULL
+        find$terms<-as.character(find$terms)
+        find_mat<-matrix(unlist(strsplit(find$terms, " ")), ncol=2, byrow=TRUE)
+        find$pred<-find_mat[,2]
+        find2<-df_unigram[grep(paste("^","\\b", clean_tail,"\\b", sep=""), df_unigram$terms),]
+        if (nrow(find2)==0){
+                warning("There is no prediction in unigram data frame")
+                return("no prediction")
+        }
+        find2<-data.frame(find2)
+        for (i in 1: nrow(find)) {
+                new_find<-data.frame("pred"=find[i,4], "pred_prob"=find[i,2]/find2[1,2])
+                merge<-rbind(merge, data.frame(new_find))
+        }
+        high_prob<-merge[order(-merge$pred_prob),]
+        possible_term<-as.character(high_prob$pred)
+        #possible_term<-data.frame(high_prob)
+        return(possible_term)
+}
+predict_trigram<-function(input){
+        if (length(input)<=1){
+                return("please enter some words")
+        }
+        input1<-tolower(input)
+        input2<-str_replace_all(input1, pattern="[[:punct:]]","") #remove punctuations
+        input3<-str_replace_all(input2, pattern="\\s+", " ") # replace whitespace with space
+        input_clean<-removeNumbers(input3) 
+        clean_tail<-tail(unlist(strsplit(input_clean, " ")), 2)
+        words<-paste(clean_tail[1], clean_tail[2], sep=" ")
+        start_with_term<-paste("^","\\b", words,"\\b", sep="")
+        find<-df_trigram[grep(start_with_term, df_trigram$terms),]
+        if (nrow(find)==0) {
+                warning("There is no prediction in trigram data frame")
+                return("no prediction")
+        }
+        merge<-NULL
+        find$terms<-as.character(find$terms)
+        find_mat<-matrix(unlist(strsplit(find$terms, " ")), ncol=3, byrow=TRUE)
+        find$pred<-find_mat[,3]
+        find2<-df_bigram[grep(paste("^","\\b",words,"\\b", sep=""), df_bigram$terms),]
+        find2<-data.frame(find2)
+        if (nrow(find2)==0) {
+                warning("There is no prediction in bigram data frame")
+                return("no prediction")
+        }
+        for (i in 1: nrow(find)) {
+                new_find<-data.frame("pred"=find[i,4], "pred_prob"=find[i,2]/find2[1,2])
+                merge<-rbind(merge, data.frame(new_find))
+        }
+        high_prob<-merge[order(-merge$pred_prob),]
+        possible_term<-as.character(high_prob$pred)
+        return(possible_term)
+}
+
+
+predict_quagram<-function(input){
+        if (length(input)<=1){
+                return("please enter some words")
+        }
+        input1<-tolower(input)
+        input2<-str_replace_all(input1, pattern="[[:punct:]]","") #remove punctuations
+        input3<-str_replace_all(input2, pattern="\\s+", " ") # replace whitespace with space
+        input_clean<-removeNumbers(input3) 
+        clean_tail<-tail(unlist(strsplit(input_clean, " ")), 3)
+        words<-paste(clean_tail[1], clean_tail[2],clean_tail[3], sep=" ")
+        start_with_term<-paste("^","\\b",words,"\\b", sep="")
+        find<-df_quagram[grep(start_with_term, df_quagram$terms),]
+        if (nrow(find)==0) {
+                warning("There is no prediction in quagram data frame")
+                return("no prediction")
+        }
+        merge<-NULL
+        find$terms<-as.character(find$terms)
+        find_mat<-matrix(unlist(strsplit(find$terms, " ")), ncol=4, byrow=TRUE)
+        find$pred<-find_mat[,4]
+        find2<-df_trigram[grep(paste("^","\\b",words,"\\b", sep=""), df_trigram$terms),]
+        find2<-data.frame(find2)
+        if (nrow(find2)==0) {
+                warning("There is no prediction in trigram data frame")
+                return("no prediction")
+        }
+        for (i in 1: nrow(find)) {
+                new_find<-data.frame("pred"=find[i,4], "pred_prob"=find[i,2]/find2[1,2])
+                merge<-rbind(merge, data.frame(new_find))
+        }
+        high_prob<-merge[order(-merge$pred_prob),]
+        possible_term<-as.character(high_prob$pred)
+        return(possible_term)
+}
+#create a funtion that can use to identify radiobutton while do the prediction: 
+nextword<-function(radiobutton, inputText,input){
+        if (input$radiobutton=="bi"){
+                predict_bigram(input$text)
+        }else if(input$radiobutton=="tri"){
+                predict_trigram(input$text)
+        }else if(input$radiobutton=="qua"){
+                predict_quagram(input$text)
+        }else{
+             "no prediction, please type some words to predict"   
+        }
+}
+
+
 
 shinyServer(function(input, output) {
-        output$value <- renderPrint({ 
-                
-                if (input$radiobutton ==  "bi"){
-                        predict_bigram(input$value)
-                }
-                else if (input$radiobutton = "Trigram prediction"){
-                        predict_trigram(input$value)
-                }
-                else (input$radiobutton = "Quadgram prediction"){
-                        predict_quagram(input$value)
-                }
-                
-                
+        radiobutton<-reactive({input$radiobutton})
+        inputText<-reactive({input$text})
+        #output$radiobutton<-render({radiobutton()})
+        output$textoutput<-renderText({
+                word<-nextword(radiobutton(),inputText,input)
+                paste0("The next word is:", word, sep=" ")
         })
-        
-     
-        
 })
 
 
--- ui.R
+++ ui.R
@@ -1,20 +1,22 @@
 library(shiny)
-library(ggplot2)
-library(markdown)
 shinyUI(navbarPage("JHU Capstone project shiny app-NLP in R",
                    tabPanel("page1-ngram prediction",
                             sidebarLayout(
                                     sidebarPanel(
                                             
-                                            textInput("test", label=h3("Text input")),
-                                            radioButtons("ngram", "ngram-prediction:",
-                                                         c("Bigram prediction"="bi",
-                                                           "Trigram prediction"="tri")),
+                                            textInput("text", label=h3("Text input"),value = ""),
+                                            radioButtons("radiobutton", label=h3("ngram-prediction:"),
+                                                         choice=list("Bigram prediction"="bi",
+                                                           "Trigram prediction"="tri",
+                                                           "Quadgram prediction"="qua")),
                                             br(),
                                             submitButton("Update"),
-                                             helpText("note: type text into text input and select which kind of ngram
+                                            helpText("note: type text into text input and select which kind of ngram
                                you want to do prediction with, and then click update button to see.
-                               For example: type :we went to new york, happi mother, etc.")),
+                               For example: type :
+                                                     we went to new york, 
+                                                     happi mother, 
+                                                     we go to see south carolina gamecock")),
                    
                                     mainPanel(
                                             
@@ -24,25 +26,8 @@ shinyUI(navbarPage("JHU Capstone project shiny app-NLP in R",
                                     )
                             )
                    ),
-                   tabPanel("page2-Wordcloud visualization",
-                            titlePanel("Wordcloud"),
-                            sidebarLayout(
-                                    sidebarPanel(
-                                            selectInput("data", "Choose a data set", c("twitter","blog","news")),
-                                            sliderInput("slider",label=h3("Word size"), min=1, max=100, value=40),
-                                            br(),
-                                            radioButtons("visual", "Visualization options:",
-                                                         c("histogram"="hist", "wordcloud"="wc")),
-                                            submitButton("Update"),
-                                            helpText("By choosing which type of visualization you 
-                                                     want, and word size, data set, you can see on the right
-                                                     the plot.")
-                                    ),
-                                    mainPanel(
-                                            h4("Plot"),
-                                            plotOutput("plot"))
-                           )),
-                   tabPanel("page3-background of NLP and references", HTML('
+                   
+                   tabPanel("page2-background of NLP and references", HTML('
                    <ol>
                     <li><a href="http://gastonsanchez.com/Handling_and_Processing_Strings_in_R.pdf">handling string in R</a></li>
                     <li><a href="http://cran.r-project.org/web/views/NaturalLanguageProcessing.html">http://CRAN NLP</a></li>                             
